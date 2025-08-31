126fe34bdf60da779374faa00c51d2fe42d79483
aravind <aravindgohan24@gmail.com>
2025-08-31 20:25:34 +0530

    reflexion agent code

--- 4.reflexion_agent/chains.py
+++ 4.reflexion_agent/chains.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
-from schema import AnswerQuestion 
+from schema import AnswerQuestion,ReviseAnswer
 from langchain_core.output_parsers import PydanticToolsParser,JsonOutputToolsParser
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
@@ -25,14 +25,24 @@ Current time: {time}
 ).partial(time=lambda:datetime.datetime.now().isoformat(),
           )
 
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
 first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Provide a detailed ~250 word answer"
 )
 llm = ChatOpenAI(model="gpt-4o")
 
-first_responder_prompt_template=first_responder_prompt_template|llm.bind_tools(tools=[AnswerQuestion],tool_choice='AnswerQuestion')|pydantic_parser
+first_responder_chain=first_responder_prompt_template|llm.bind_tools(tools=[AnswerQuestion],tool_choice='AnswerQuestion')|pydantic_parser
 
-response=first_responder_prompt_template.invoke({
+revisor_chain =first_responder_prompt_template.partial(first_instruction=revise_instructions)|llm.bind_tools(tools=[ReviseAnswer],tool_choice="ReviseAnswer")
+response=first_responder_chain.invoke({
     "messages":[HumanMessage("AI Agents taking over content creation"
                              )]
                              })
